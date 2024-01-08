b666177510ebf8d76daf1bab6562e2527a18b8c8
jknadan <jknadan@gmail.com>
2024-01-08 23:51:04 +0900

    error fix

--- src/app/User/userService.js
+++ src/app/User/userService.js
@@ -15,10 +15,15 @@ const {connect} = require("http2");
 
 exports.createUser = async function (name, group, phone, ID, password) {
     try {
-        // 이메일 중복 확인
-        const emailRows = await userProvider.emailCheck(email);
+
+        const userRows = await userProvider.userCheck(name);
+        if (userRows.length <= 0)
+        return errResponse()
+
+        // // 이메일 중복 확인
+        // const emailRows = await userProvider.emailCheck(email);
         if (emailRows.length > 0)
-            return errResponse(baseResponse.SIGNUP_REDUNDANT_EMAIL);
+            return errResponse({ "isSuccess": false, "code": 3001, "message":"중복된 이메일입니다." });
 
         // 비밀번호 암호화
         const hashedPassword = await crypto
