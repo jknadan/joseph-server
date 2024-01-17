c895042dae867237e85c4ab43c440b14fb3b54c4
jknadan <jknadan@gmail.com>
2024-01-17 23:14:45 +0900

    [addFeat]: jwtToken Logic Add

--- src/app/User/userDao.js
+++ src/app/User/userDao.js
@@ -1,3 +1,4 @@
+/** 
 // 모든 유저 조회
 async function selectUser(connection) {
   const selectUserListQuery = `
@@ -29,7 +30,7 @@ async function selectUserId(connection, userId) {
   const [userRow] = await connection.query(selectUserIdQuery, userId);
   return userRow;
 }
-
+**/
 // 유저 생성
 async function insertUserInfo(connection, insertUserInfoParams) {
 
@@ -45,42 +46,6 @@ async function insertUserInfo(connection, insertUserInfoParams) {
   return insertUserInfoRow;
 }
 
-// 패스워드 체크
-async function selectUserPassword(connection, selectUserPasswordParams) {
-  const selectUserPasswordQuery = `
-        SELECT email, nickname, password
-        FROM UserInfo 
-        WHERE email = ? AND password = ?;`;
-  const selectUserPasswordRow = await connection.query(
-      selectUserPasswordQuery,
-      selectUserPasswordParams
-  );
-
-  return selectUserPasswordRow;
-}
-
-// 유저 계정 상태 체크 (jwt 생성 위해 id 값도 가져온다.)
-async function selectUserAccount(connection, email) {
-  const selectUserAccountQuery = `
-        SELECT status, id
-        FROM UserInfo 
-        WHERE email = ?;`;
-  const selectUserAccountRow = await connection.query(
-      selectUserAccountQuery,
-      email
-  );
-  return selectUserAccountRow[0];
-}
-
-async function updateUserInfo(connection, id, nickname) {
-  const updateUserQuery = `
-  UPDATE UserInfo 
-  SET nickname = ?
-  WHERE id = ?;`;
-  const updateUserRow = await connection.query(updateUserQuery, [nickname, id]);
-  return updateUserRow[0];
-}
-
 async function checkUserLoginInfo(connection, ID, password){
   const checkUserInfoQuery = `
   select UserID 
@@ -92,14 +57,17 @@ async function checkUserLoginInfo(connection, ID, password){
 
 }
 
+async function saveRefreshToken(connection,refreshToken){
+  const saveRefreshTokenQuery = `
+  INSERT INTO User(RefreshToken) VALUES (?);
+  `;
+
+  const userSaveInfo = await connection.query(saveRefreshTokenQuery,refreshToken);
+  return userSaveInfo[0];
+}
+
 
 module.exports = {
-  selectUser,
-  selectUserEmail,
-  selectUserId,
-  insertUserInfo,
-  selectUserPassword,
-  selectUserAccount,
-  updateUserInfo,
-  checkUserLoginInfo
+  checkUserLoginInfo,
+  saveRefreshToken
 };
--- src/app/User/userRoute.js
+++ src/app/User/userRoute.js
@@ -11,7 +11,6 @@ module.exports = function(app){
     // 2. 유저 조회 API (+ 검색)
     app.get('/app/users',user.getUsers); 
 
-
     // 3. 유저 로그인
     app.post('/login', user.userLogin);
 
@@ -19,9 +18,9 @@ module.exports = function(app){
     app.get('/app/users/:userId', user.getUserById);
 
 
-    // TODO: After 로그인 인증 방법 (JWT)
-    // 로그인 하기 API (JWT 생성)
-    app.post('/app/login', user.login);
+    // // TODO: After 로그인 인증 방법 (JWT)
+    // // 로그인 하기 API (JWT 생성)
+    // app.post('/app/login', user.login);
 
     // 회원 정보 수정 API (JWT 검증 및 Validation - 메소드 체이닝 방식으로 jwtMiddleware 사용)
     app.patch('/app/users/:userId', jwtMiddleware, user.patchUsers)
